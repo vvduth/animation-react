af8430fd1a29d57e479c3d581426ee1893a80975
vvduth <ducthai060501@gmail.com>
2025-11-21 14:27:54 +0200

    Refactor Header component to use motion.button for enhanced animations and improve code consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import { AnimatePresence } from 'motion/react';
-import { useState } from 'react';
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -16,16 +16,31 @@ export default function Header() {
 
   return (
     <>
-    
       <AnimatePresence>
         {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
       </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button onClick={handleStartAddNewChallenge}
+        // i will not use animate here
+        // cuz it would mean i would have to listen to 
+        // onHoverStart and onHoverEnd event for the button to pop out libit
+        // that alot of work for a small effect
+
+        // use whileHover instead for simplicity
+        whileHover={{
+          scale: 1.1
+        }}
+        // transition will controll all the animation behavior applyon this element
+        transition={{
+          type: "spring",
+          stiffness: 400,
+          mass: 0.5,
+        }}
+        className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
