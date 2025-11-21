b61c51d60bb5369378f3dba7b5ef39957e1db681
vvduth <ducthai060501@gmail.com>
2025-11-21 14:34:41 +0200

    Refactor Modal component to use motion.dialog with defined animation states for improved clarity and consistency

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,15 +2,21 @@ import { createPortal } from "react-dom";
 import { motion } from "motion/react";
 
 export default function Modal({ title, children, onClose }) {
+  
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
+      // useful for defining and resueing animation states
+      variants={{
+        hidden: { opacity: 0, y: 30 },
+        visible: { opacity: 1, y: 0 }
+      }}
         open
         className="modal"
-        animate={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: 30 }}
-        exit={{ opacity: 0, y: 30 }}
+        animate="visible"
+        initial="hidden"
+        exit="hidden"
       >
         <h2>{title}</h2>
         {children}
