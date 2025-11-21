d5310c815419be70dfb534abcd4f16e7f376b999
vvduth <ducthai060501@gmail.com>
2025-11-21 13:34:44 +0200

    Refactor Modal component to use motion.dialog for animations and remove unused CSS animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,21 @@
-import { createPortal } from 'react-dom';
-
+import { createPortal } from "react-dom";
+import { motion } from "motion/react";
 export default function Modal({ title, children, onClose }) {
+  // we have so prop or statre in here, we doesnt need them cuz framer
+  // have some prop to handle animation without state
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        open
+        className="modal"
+        animate={{ opacity: 1, y: 0 }} // animate prop to set the final state of the animation
+        initial={{ opacity: 0, y: 30 }} // inital prop to set the initial state of the animation
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
